e4f587b88f26237d49ac26b7b4f13a7ca8d3187d
KIBASSA MALIBA JUNIOR <juniorkibassa2007@gmail.com>
2019-12-09 20:11:04 +0100

    update

--- docker_client/src/Face_Recognition_and_Identification.py
+++ docker_client/src/Face_Recognition_and_Identification.py
@@ -1,5 +1,7 @@
 #https://www.framboise314.fr/i-a-realisez-un-systeme-de-reconnaissance-dobjets-avec-raspberry-pi/ in case xd
 # tuto face recognition (18 images xd ) : https://www.pyimagesearch.com/2018/09/24/opencv-face-recognition/
+
+"""
 import numpy as np
 import cv2 #pip install opencv-python
 import dlib #conda install -c conda-forge dlib
@@ -29,7 +31,7 @@ while(cap.isOpened()): # check if my Videocapture is open
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-        landmarks = predictor(gray, face)
+        landmarks = predictor(gray, (x,y,w,h))
 
         #authent_users = mongo_api.get_users()
         #if len(authent_users) > 0:
@@ -49,3 +51,39 @@ while(cap.isOpened()): # check if my Videocapture is open
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
+"""
+import cv2
+import numpy as np
+import dlib
+
+cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+while True:
+    _, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+    for face in faces:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+        landmarks = predictor(gray, face)
+        #print(landmarks)
+        for n in range(0, 68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            cv2.circle(frame, (x, y), 4, (255, 0, 0), -1)
+            print(x,"----",y)
+
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
---
+++ docker_client/src/module_speaker_detection.py
@@ -0,0 +1,1 @@
+#https://github.com/Microsoft/Cognitive-SpeakerRecognition-Python
