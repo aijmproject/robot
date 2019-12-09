c11a829a58a87cf04a937852c4b4631d635e6498
KIBASSA MALIBA JUNIOR <juniorkibassa2007@gmail.com>
2019-12-09 19:30:28 +0100

    update

--- docker_client/src/module_speech_recognition.py
+++ docker_client/src/module_speech_recognition.py
@@ -10,8 +10,8 @@
 import azure.cognitiveservices.speech as speechsdk
 # Creates an instance of a speech config with specified subscription key and service region.
 # Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "7312fdabc95d4f61b94408d74612f4ea", "France - Centre"
-speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+speech_key, service_region = "7312fdabc95d4f61b94408d74612f4ea", "francecentral"
+speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region, speech_recognition_language='fr-FR')
 
 # Creates a recognizer with the given settings
 speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
@@ -19,6 +19,8 @@ speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 print("Say something...")
 
 result = speech_recognizer.recognize_once()
+
+print("test 2")
 
 # Checks result.
 if result.reason == speechsdk.ResultReason.RecognizedSpeech:
