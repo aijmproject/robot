1a6b13435fff45a3eea67fa99ccc64e806029c80
KIBASSA MALIBA JUNIOR <juniorkibassa2007@gmail.com>
2019-12-14 20:18:28 +0100

    add SpeechToText's class to handle speech to text

--- docker_client/src/module_speech_recognition.py
+++ docker_client/src/module_speech_recognition.py
@@ -8,27 +8,41 @@
 #    print(r.recognize_google(audio))
 
 import azure.cognitiveservices.speech as speechsdk
-# Creates an instance of a speech config with specified subscription key and service region.
-# Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "7312fdabc95d4f61b94408d74612f4ea", "francecentral"
-speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region, speech_recognition_language='fr-FR')
 
-# Creates a recognizer with the given settings
-speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
 
-print("Say something...")
+class SpeechToText:
+    def __init__(self):
+        self.speech_key = "7312fdabc95d4f61b94408d74612f4ea"
+        self.service_region = "francecentral"
+        self.speech_config = speechsdk.SpeechConfig(subscription=self.speech_key, region=self.service_region, speech_recognition_language='fr-FR')
 
-result = speech_recognizer.recognize_once()
 
-print("test 2")
+    def get_text(self):
+        # Creates an instance of a speech config with specified subscription key and service region.
+        # Replace with your own subscription key and service region (e.g., "westus").
+        speech_key, service_region = "7312fdabc95d4f61b94408d74612f4ea", "francecentral"
+        speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region, speech_recognition_language='fr-FR')
 
-# Checks result.
-if result.reason == speechsdk.ResultReason.RecognizedSpeech:
-    print("Recognized: {}".format(result.text))
-elif result.reason == speechsdk.ResultReason.NoMatch:
-    print("No speech could be recognized: {}".format(result.no_match_details))
-elif result.reason == speechsdk.ResultReason.Canceled:
-    cancellation_details = result.cancellation_details
-    print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-    if cancellation_details.reason == speechsdk.CancellationReason.Error:
-        print("Error details: {}".format(cancellation_details.error_details))
+        # Creates a recognizer with the given settings
+        speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config)
+
+        result = speech_recognizer.recognize_once()
+
+        # Checks result.
+        text_result = ""
+        if result.reason == speechsdk.ResultReason.RecognizedSpeech:
+            print("Recognized: {}".format(result.text))
+            text_result = result.text
+        elif result.reason == speechsdk.ResultReason.NoMatch:
+            print("No speech could be recognized: {}".format(result.no_match_details))
+        elif result.reason == speechsdk.ResultReason.Canceled:
+            cancellation_details = result.cancellation_details
+            print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+            if cancellation_details.reason == speechsdk.CancellationReason.Error:
+                print("Error details: {}".format(cancellation_details.error_details))
+        
+        return text_result
+
+if __name__ == "__main__":
+    app = SpeechToText()
+    app.get_text()
