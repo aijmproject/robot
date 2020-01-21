cee86746cd4e58fccd6fac091b962e18ceb7d132
Matthew <matthew.iqbal@u-psud.fr>
2020-01-21 17:13:12 +0100

    face-recognition

---
+++ docker_client/src/face-recognition/surveillance_class.py
@@ -0,0 +1,17 @@
+
+from extract_embeddings import extract_embeddings
+from recognize_video import recognize_video
+from train_model import train_model
+
+
+class FaceDetection():
+
+    def __init__(self):
+        pass
+    def full_model() :
+        extract_embeddings()
+	    train_model()
+    def run_video(video_input):
+        recognize_video(video_input)
+    
+
